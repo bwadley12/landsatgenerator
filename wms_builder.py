30fcb2bc67288ba8187d7e039d215ab576604f4d
bwadley12 <brad.willimack@gmail.com>
2024-01-06 13:23:27 -0700

    added multithreading to wms builder, now much much faster

--- wms_builder.py
+++ wms_builder.py
@@ -2,8 +2,9 @@ from os import listdir, makedirs, path
 from sys import argv
 import requests
 import shutil
+from concurrent.futures import ThreadPoolExecutor
 
-VALID_LEVELS = ["0","1","2","3","4","5"] 
+VALID_LEVELS = ["0","1","2","3","4","5","6"] 
 EXPECTED_ARGUMENT_LENGTH = 2
 LEVEL = None
 
@@ -13,7 +14,8 @@ rows_per_level = {
     "2": 20,
     "3": 40,
     "4": 80,
-    "5": 160
+    "5": 160,
+    "6": 320
 }
 
 columns_per_level = {
@@ -22,7 +24,8 @@ columns_per_level = {
     "2": 40,
     "3": 80,
     "4": 160,
-    "5": 320
+    "5": 320,
+    "6": 640
 }
 
 degrees_per_image_per_level = {
@@ -31,7 +34,8 @@ degrees_per_image_per_level = {
     "2": 9,
     "3": 4.5,
     "4": 2.25,
-    "5": 1.125
+    "5": 1.125,
+    "6": 0.5625
 }
 
 URL = 'https://worldwind25.arc.nasa.gov/wms'
@@ -51,6 +55,19 @@ parameters = {
     "crs":"EPSG:4326",
     "bbox":"54,-180,90,-144",
 }
+
+
+def getImage(URL, parameters, HEADERS, image_name):
+    response = requests.get(URL, 
+                                params=parameters,
+                                headers=HEADERS, 
+                                stream=True)
+
+    if response.status_code == 200:
+        with open(image_name, 'wb') as f:
+            response.raw.decode_content = True
+            shutil.copyfileobj(response.raw, f)
+
 
 # ingest argv, error check it. Should give 1 additional args, level value from 0 - 9
 if len(argv) > EXPECTED_ARGUMENT_LENGTH:
@@ -82,29 +99,23 @@ for path_to_create in paths_to_create:
 
 
 thisrow = 0
-while thisrow < rows_per_level[LEVEL]:
-    thiscolumn = 0
-    while thiscolumn < columns_per_level[LEVEL]:
-        lower_lat = -90 + thisrow * degrees_per_image_per_level[LEVEL]
-        lower_long = -180 + thiscolumn * degrees_per_image_per_level[LEVEL]
-        upper_lat = lower_lat + degrees_per_image_per_level[LEVEL]
-        upper_long = lower_long + degrees_per_image_per_level[LEVEL]
-        bbox = str(lower_lat) + "\," + str(lower_long) + "\," + str(upper_lat) + "\," + str(upper_long)
-        parameters["bbox"] = bbox
-        image_name = path.join(".", LEVEL, str(thisrow), str(thisrow) + "_" + str(thiscolumn) + ".png")
 
-        response = requests.get(URL, 
-                        params=parameters,
-                        headers=HEADERS, 
-                        stream=True)
+with ThreadPoolExecutor(max_workers = 100) as e:
+    while thisrow < rows_per_level[LEVEL]:
+        thiscolumn = 0
+        while thiscolumn < columns_per_level[LEVEL]:
+            lower_lat = -90 + thisrow * degrees_per_image_per_level[LEVEL]
+            lower_long = -180 + thiscolumn * degrees_per_image_per_level[LEVEL]
+            upper_lat = lower_lat + degrees_per_image_per_level[LEVEL]
+            upper_long = lower_long + degrees_per_image_per_level[LEVEL]
+            bbox = str(lower_lat) + "\," + str(lower_long) + "\," + str(upper_lat) + "\," + str(upper_long)
+            parameters["bbox"] = bbox
+            image_name = path.join(".", LEVEL, str(thisrow), str(thisrow) + "_" + str(thiscolumn) + ".png")
 
-        if response.status_code == 200:
-            with open(image_name, 'wb') as f:
-                response.raw.decode_content = True
-                shutil.copyfileobj(response.raw, f)
-
-        thiscolumn += 1
-    thisrow += 1
+            e.submit(getImage, URL, dict(parameters), HEADERS, image_name)
+                
+            thiscolumn += 1
+        thisrow += 1
 
 
 
